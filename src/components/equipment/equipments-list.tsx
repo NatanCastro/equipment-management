56dc34e953ab948a4dc1f2295155e31e9466c056
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2025-02-18 01:45:52 -0300

    change equipment list to a 3 column format

--- src/components/equipment/equipments-list.tsx
+++ src/components/equipment/equipments-list.tsx
@@ -9,7 +9,7 @@ type EquipmentsListProps = {
 export function EquipmentsList({ equipments }: EquipmentsListProps) {
   return (
     <div className="p-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {equipments.map((item) => (
           <EquipmentCard key={item.id} equipment={item} />
         ))}
