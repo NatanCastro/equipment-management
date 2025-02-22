ba126c3a4b48a4c4229f5adee3f2f445fd1fe0a9
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2025-02-22 16:58:40 -0300

    create a table for the location of the equipment

---
+++ src-tauri/migrations/20250222194922_create-equipment-location-table.sql
@@ -0,0 +1,13 @@
+-- Add migration script here
+CREATE TABLE equipment_location (
+    id UUID PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at TEXT NOT NULL default (datetime('now')),
+    updated_at TEXT NOT NULL default (datetime('now')),
+);
+
+CREATE INDEX IF NOT EXISTS equipment_location_name_index ON equipment_location (name);
+
+ALTER TABLE equipment
+ADD COLUMN equipment_location_id UUID REFERENCES equipment_location (id);
