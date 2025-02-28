206e718976171159e87caa3e2d34212ffda1e908
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2025-02-28 19:43:33 -0300

    update result type to use a tag instead of a boolean value

--- src/types/result.ts
+++ src/types/result.ts
@@ -1,29 +1,29 @@
 
 type Ok<T, _> = {
-  ok: true;
+  tag: "ok";
   val: T;
 };
 
 type Err<_, E> = {
-  ok: false;
+  tag: "err";
   err: E;
 };
 
 export type Result<T, E = Error> = Ok<T, E> | Err<T, E>;
 
 export const ok = <T, E = Error>(val: T): Result<T, E> => ({
-  ok: true,
+  tag: "ok",
   val,
 });
 
 export const err = <T, E = Error>(err: E): Result<T, E> => ({
-  ok: false,
+  tag: "err",
   err,
 });
 
-export const isOk = <T, E = Error>(result: Result<T, E>): result is Ok<T, E> => result.ok;
+export const isOk = <T, E = Error>(result: Result<T, E>): result is Ok<T, E> => result.tag === "ok";
 
-export const isErr = <T, E = Error>(result: Result<T, E>): result is Err<T, E> => !result.ok;
+export const isErr = <T, E = Error>(result: Result<T, E>): result is Err<T, E> => result.tag === "err";
 
 export const unwrap = <T, E = Error>(result: Result<T, E>): T => {
   if (isErr(result)) {
